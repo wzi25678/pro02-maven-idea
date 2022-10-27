02a3b7651df6b6159dcf8833bf13f4807e975c79
wzi <2567839929@qq.com>
2022-10-27 20:37:15 +0800

    Forth Commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
         String s1= "ffffffffff";
+        System.out.println("forth twice!");
     }
 }
